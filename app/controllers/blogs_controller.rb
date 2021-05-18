00347d7f6b54cb31109152cbee054dc47f76ac5e
BachShinichiroe <kitchen.number@gmail.com>
2021-05-19 00:48:12 +0900

    ✨add:NEWS投稿機能

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,18 @@
+class BlogsController < ApplicationController
+  def index
+    @blogs = Blog.all
+  end
+  def new
+    @blog = Blog.new
+  end
+  def create
+    Blog.create(blog_params)
+    redirect_to tops_path
+  end
+  
+  
+  private
+  def blog_params
+    params.require(:blog).permit(:title, :content)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   get 'audition/index'
   get 'about/index'
   resources :tops
+  resources :blogs
+  resources :news
   root 'tops#index'
 end
